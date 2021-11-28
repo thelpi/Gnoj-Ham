7959a52ddee6d2a9d9aa45bfcd6b08aa755b0a22
Lpi <thelpi.v3@gmail.com>
2021-11-28 15:09:04 +0100

    Options on home screen

--- Gnoj-HamView/IntroWindow.xaml.cs
+++ Gnoj-HamView/IntroWindow.xaml.cs
@@ -19,14 +19,14 @@ namespace Gnoj_HamView
 
 #if DEBUG
             GrbDebugOptions.Visibility = Visibility.Visible;
-#else
-            GrbDebugOptions.Visibility = Visibility.Collapsed;
 #endif
 
             LoadConfiguration();
 
             CbbEndOfGameRule.ItemsSource = GraphicTools.GetEndOfGameRuleDisplayValue();
             CbbPointsRule.ItemsSource = GraphicTools.GetInitialPointsRuleDisplayValue();
+            CbbChronoSpeed.ItemsSource = GraphicTools.GetChronoDisplayValues();
+            CbbCpuSpeed.ItemsSource = GraphicTools.GetCpuSpeedDisplayValues();
         }
 
         #region Page events
@@ -63,24 +63,49 @@ namespace Gnoj_HamView
 
         private void LoadConfiguration()
         {
+            // Rules
             CbbPointsRule.SelectedIndex = Settings.Default.InitialPointsRule;
             CbbEndOfGameRule.SelectedIndex = Settings.Default.EndOfGameRule;
-            TxtPlayerName.Text = Settings.Default.DefaultPlayerName;
             ChkUseRedDoras.IsChecked = Settings.Default.DefaultUseRedDoras;
-            ChkDebugMode.IsChecked = Settings.Default.DebugMode;
             ChkUseNagashiMangan.IsChecked = Settings.Default.UseNagashiMangan;
             ChkUseRenhou.IsChecked = Settings.Default.UseRenhou;
+
+            // Options
+            TxtPlayerName.Text = Settings.Default.DefaultPlayerName;
+            CbbChronoSpeed.SelectedIndex = Settings.Default.ChronoSpeed;
+            CbbCpuSpeed.SelectedIndex = Settings.Default.CpuSpeed;
+            ChkSounds.IsChecked = Settings.Default.PlaySounds;
+            ChkAutoTsumoRon.IsChecked = Settings.Default.AutoCallMahjong;
+            ChkAutoRiichiDiscard.IsChecked = Settings.Default.AutoDiscardAfterRiichi;
+
+            // Debug
+#if DEBUG
+            ChkDebugMode.IsChecked = Settings.Default.DebugMode;
+#endif
         }
 
         private void SaveConfiguration()
         {
+            // Rules
             Settings.Default.InitialPointsRule = CbbPointsRule.SelectedIndex;
             Settings.Default.EndOfGameRule = CbbEndOfGameRule.SelectedIndex;
-            Settings.Default.DefaultPlayerName = TxtPlayerName.Text;
             Settings.Default.DefaultUseRedDoras = ChkUseRedDoras.IsChecked == true;
-            Settings.Default.DebugMode = ChkDebugMode.IsChecked == true;
             Settings.Default.UseNagashiMangan = ChkUseNagashiMangan.IsChecked == true;
             Settings.Default.UseRenhou = ChkUseRenhou.IsChecked == true;
+
+            // Options
+            Settings.Default.DefaultPlayerName = TxtPlayerName.Text;
+            Settings.Default.ChronoSpeed = CbbChronoSpeed.SelectedIndex;
+            Settings.Default.CpuSpeed = CbbCpuSpeed.SelectedIndex;
+            Settings.Default.PlaySounds = ChkSounds.IsChecked == true;
+            Settings.Default.AutoCallMahjong = ChkAutoTsumoRon.IsChecked == true;
+            Settings.Default.AutoDiscardAfterRiichi = ChkAutoRiichiDiscard.IsChecked == true;
+
+            // Debug
+#if DEBUG
+            Settings.Default.DebugMode = ChkDebugMode.IsChecked == true;
+#endif
+
             Settings.Default.Save();
         }
 
