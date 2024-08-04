9d7a0b7778bbfe06588333bbc46a929faf215955
Lpi <thelpi.v3@gmail.com>
2024-08-04 18:13:54 +0200

    scoretools ok

--- Gnoj-Ham-Library/ScoreTools.cs
+++ Gnoj-Ham-Library/ScoreTools.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Gnoj_Ham_Library.Enums;
+﻿using Gnoj_Ham_Library.Enums;
 
 namespace Gnoj_Ham_Library
 {
@@ -34,7 +31,7 @@ namespace Gnoj_Ham_Library
         private const int BASE_CONCEALED_RON_FU = 30;
 
         // Minimal fan count / point lost by each opponent (x2 for east, or x3 for ron on a single player)
-        private static readonly Dictionary<int, int> OVER_FOUR_FAN = new Dictionary<int, int>
+        private static readonly IReadOnlyDictionary<int, int> OVER_FOUR_FAN = new Dictionary<int, int>
         {
             { 5, 2000 },
             { 6, 3000 },
@@ -44,75 +41,75 @@ namespace Gnoj_Ham_Library
         };
 
         // Minimal fan count / Minimal fu count / points lost by the ron opponent / points lost by east if tsumo / points lost by others if tsumo
-        private static readonly IReadOnlyList<Tuple<int, int, int, int, int>> CHART_OTHER = new List<Tuple<int, int, int, int, int>>
+        private static readonly IReadOnlyList<(int, int, int, int, int)> CHART_OTHER = new List<(int, int, int, int, int)>
         {
             // 1-20 and 1-25 are impossible
-            new Tuple<int, int, int, int, int>(1, 030, 1000, 0500, 0300),
-            new Tuple<int, int, int, int, int>(1, 040, 1300, 0700, 0400),
-            new Tuple<int, int, int, int, int>(1, 050, 1600, 0800, 0400),
-            new Tuple<int, int, int, int, int>(1, 060, 2000, 1000, 0500),
-            new Tuple<int, int, int, int, int>(1, 070, 2300, 1200, 0600),
-            new Tuple<int, int, int, int, int>(1, 080, 2600, 1300, 0700),
-            new Tuple<int, int, int, int, int>(1, 090, 2900, 1500, 0800),
-            new Tuple<int, int, int, int, int>(1, 100, 3200, 1600, 0800),
-            new Tuple<int, int, int, int, int>(1, 110, 3600, 1800, 0900),
-            new Tuple<int, int, int, int, int>(2, 020, 1300, 0700, 0400),
+            (1, 030, 1000, 0500, 0300),
+            (1, 040, 1300, 0700, 0400),
+            (1, 050, 1600, 0800, 0400),
+            (1, 060, 2000, 1000, 0500),
+            (1, 070, 2300, 1200, 0600),
+            (1, 080, 2600, 1300, 0700),
+            (1, 090, 2900, 1500, 0800),
+            (1, 100, 3200, 1600, 0800),
+            (1, 110, 3600, 1800, 0900),
+            (2, 020, 1300, 0700, 0400),
             // 2 fans chiitoi is impossible in tsumo.
-            new Tuple<int, int, int, int, int>(2, 025, 1600, 0000, 0000),
-            new Tuple<int, int, int, int, int>(2, 030, 2000, 1000, 0500),
-            new Tuple<int, int, int, int, int>(2, 040, 2600, 1300, 0700),
-            new Tuple<int, int, int, int, int>(2, 050, 3200, 1600, 0800),
-            new Tuple<int, int, int, int, int>(2, 060, 3900, 2000, 1000),
-            new Tuple<int, int, int, int, int>(2, 070, 4500, 2300, 1200),
-            new Tuple<int, int, int, int, int>(2, 080, 5200, 2600, 1300),
-            new Tuple<int, int, int, int, int>(2, 090, 5800, 2900, 1500),
-            new Tuple<int, int, int, int, int>(2, 100, 6400, 3200, 1600),
-            new Tuple<int, int, int, int, int>(2, 110, 7100, 3600, 1800),
-            new Tuple<int, int, int, int, int>(3, 020, 2600, 1300, 0700),
-            new Tuple<int, int, int, int, int>(3, 025, 3200, 1600, 0800),
-            new Tuple<int, int, int, int, int>(3, 030, 3900, 2000, 1000),
-            new Tuple<int, int, int, int, int>(3, 040, 5200, 2600, 1300),
-            new Tuple<int, int, int, int, int>(3, 050, 6400, 3200, 1600),
-            new Tuple<int, int, int, int, int>(3, 060, 7700, 3900, 2000),
-            new Tuple<int, int, int, int, int>(4, 020, 5200, 2600, 1300),
-            new Tuple<int, int, int, int, int>(4, 025, 6400, 3200, 1600),
-            new Tuple<int, int, int, int, int>(4, 030, 7700, 3900, 2000)
+            (2, 025, 1600, 0000, 0000),
+            (2, 030, 2000, 1000, 0500),
+            (2, 040, 2600, 1300, 0700),
+            (2, 050, 3200, 1600, 0800),
+            (2, 060, 3900, 2000, 1000),
+            (2, 070, 4500, 2300, 1200),
+            (2, 080, 5200, 2600, 1300),
+            (2, 090, 5800, 2900, 1500),
+            (2, 100, 6400, 3200, 1600),
+            (2, 110, 7100, 3600, 1800),
+            (3, 020, 2600, 1300, 0700),
+            (3, 025, 3200, 1600, 0800),
+            (3, 030, 3900, 2000, 1000),
+            (3, 040, 5200, 2600, 1300),
+            (3, 050, 6400, 3200, 1600),
+            (3, 060, 7700, 3900, 2000),
+            (4, 020, 5200, 2600, 1300),
+            (4, 025, 6400, 3200, 1600),
+            (4, 030, 7700, 3900, 2000)
         };
 
         // Minimal fan count / Minimal fu count / points lost by the ron opponent / points lost by east if tsumo / points lost by others if tsumo
-        private static readonly IReadOnlyList<Tuple<int, int, int, int>> CHART_EAST = new List<Tuple<int, int, int, int>>
+        private static readonly IReadOnlyList<(int, int, int, int)> CHART_EAST = new List<(int, int, int, int)>
         {
             // 1-20 and 1-25 are impossible
-            new Tuple<int, int, int, int>(1, 030, 1500, 0500),
-            new Tuple<int, int, int, int>(1, 040, 2000, 0700),
-            new Tuple<int, int, int, int>(1, 050, 2400, 0800),
-            new Tuple<int, int, int, int>(1, 060, 2900, 1000),
-            new Tuple<int, int, int, int>(1, 070, 3400, 1200),
-            new Tuple<int, int, int, int>(1, 080, 3900, 1300),
-            new Tuple<int, int, int, int>(1, 090, 4400, 1500),
-            new Tuple<int, int, int, int>(1, 100, 4800, 1600),
-            new Tuple<int, int, int, int>(1, 110, 5300, 1800),
-            new Tuple<int, int, int, int>(2, 020, 2000, 0700),
+            (1, 030, 1500, 0500),
+            (1, 040, 2000, 0700),
+            (1, 050, 2400, 0800),
+            (1, 060, 2900, 1000),
+            (1, 070, 3400, 1200),
+            (1, 080, 3900, 1300),
+            (1, 090, 4400, 1500),
+            (1, 100, 4800, 1600),
+            (1, 110, 5300, 1800),
+            (2, 020, 2000, 0700),
             // 2 fans chiitoi is impossible in tsumo.
-            new Tuple<int, int, int, int>(2, 025, 2400, 0000),
-            new Tuple<int, int, int, int>(2, 030, 2900, 1000),
-            new Tuple<int, int, int, int>(2, 040, 3900, 1300),
-            new Tuple<int, int, int, int>(2, 050, 4800, 1600),
-            new Tuple<int, int, int, int>(2, 060, 5800, 2000),
-            new Tuple<int, int, int, int>(2, 070, 6800, 2300),
-            new Tuple<int, int, int, int>(2, 080, 7700, 2600),
-            new Tuple<int, int, int, int>(2, 090, 8700, 2900),
-            new Tuple<int, int, int, int>(2, 100, 9600, 3200),
-            new Tuple<int, int, int, int>(2, 110, 10600, 3600),
-            new Tuple<int, int, int, int>(3, 020, 3900, 1300),
-            new Tuple<int, int, int, int>(3, 025, 4800, 1600),
-            new Tuple<int, int, int, int>(3, 030, 5800, 2000),
-            new Tuple<int, int, int, int>(3, 040, 7700, 2600),
-            new Tuple<int, int, int, int>(3, 050, 9600, 3200),
-            new Tuple<int, int, int, int>(3, 060, 11600, 3900),
-            new Tuple<int, int, int, int>(4, 020, 7700, 2600),
-            new Tuple<int, int, int, int>(4, 025, 9600, 3200),
-            new Tuple<int, int, int, int>(4, 030, 11600, 3900)
+            (2, 025, 2400, 0000),
+            (2, 030, 2900, 1000),
+            (2, 040, 3900, 1300),
+            (2, 050, 4800, 1600),
+            (2, 060, 5800, 2000),
+            (2, 070, 6800, 2300),
+            (2, 080, 7700, 2600),
+            (2, 090, 8700, 2900),
+            (2, 100, 9600, 3200),
+            (2, 110, 10600, 3600),
+            (3, 020, 3900, 1300),
+            (3, 025, 4800, 1600),
+            (3, 030, 5800, 2000),
+            (3, 040, 7700, 2600),
+            (3, 050, 9600, 3200),
+            (3, 060, 11600, 3900),
+            (4, 020, 7700, 2600),
+            (4, 025, 9600, 3200),
+            (4, 030, 11600, 3900)
         };
 
         #endregion Points chart
@@ -122,13 +119,13 @@ namespace Gnoj_Ham_Library
         /// </summary>
         /// <param name="countTenpai">Count of tenpai players.</param>
         /// <returns>Points for tenpai players; Points for non-tenpai players.</returns>
-        internal static Tuple<int, int> GetRyuukyokuPoints(int countTenpai)
+        internal static (int tenpai, int nonTenpai) GetRyuukyokuPoints(int countTenpai)
         {
             return countTenpai == 1
-                ? new Tuple<int, int>(TENPAI_BASE_POINTS * (4 - countTenpai), -TENPAI_BASE_POINTS)
+                ? (TENPAI_BASE_POINTS * (4 - countTenpai), -TENPAI_BASE_POINTS)
                 : countTenpai == 2
-                    ? new Tuple<int, int>(TENPAI_BASE_POINTS + TENPAI_BASE_POINTS / countTenpai, -(TENPAI_BASE_POINTS + TENPAI_BASE_POINTS / countTenpai))
-                    : countTenpai == 3 ? new Tuple<int, int>(TENPAI_BASE_POINTS, countTenpai * -TENPAI_BASE_POINTS) : new Tuple<int, int>(0, 0);
+                    ? (TENPAI_BASE_POINTS + TENPAI_BASE_POINTS / countTenpai, -(TENPAI_BASE_POINTS + TENPAI_BASE_POINTS / countTenpai))
+                    : countTenpai == 3 ? (TENPAI_BASE_POINTS, countTenpai * -TENPAI_BASE_POINTS) : (0, 0);
         }
 
         /// <summary>
@@ -140,14 +137,8 @@ namespace Gnoj_Ham_Library
         /// <param name="uraDorasCount">Optionnal; ura-doras count.</param>
         /// <param name="redDorasCount">Optionnal; red doras count.</param>
         /// <returns>The fan count.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="yakus"/> is <c>Null</c>.</exception>
         internal static int GetFanCount(IReadOnlyList<YakuPivot> yakus, bool concealed, int dorasCount = 0, int uraDorasCount = 0, int redDorasCount = 0)
         {
-            if (yakus == null)
-            {
-                throw new ArgumentNullException(nameof(yakus));
-            }
-
             var yakumansCount = yakus.Count(y => (concealed ? y.ConcealedFanCount : y.FanCount) == 13);
 
             if (yakumansCount > 0)
@@ -167,14 +158,8 @@ namespace Gnoj_Ham_Library
         /// <param name="isTsumo"><c>True</c> if the winning tile is concealed; <c>False</c> otherwise.</param>
         /// <param name="dominantWind">The dominant wind;</param>
         /// <param name="playerWind">The player wind.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="hand"/> is <c>Null</c>.</exception>
         internal static int GetFuCount(HandPivot hand, bool isTsumo, Winds dominantWind, Winds playerWind)
         {
-            if (hand == null)
-            {
-                throw new ArgumentNullException(nameof(hand));
-            }
-
             if (hand.Yakus.Any(y => y == YakuPivot.Chiitoitsu))
             {
                 return CHIITOI_FU;
@@ -222,7 +207,7 @@ namespace Gnoj_Ham_Library
         /// - Number of points lost by east players (or one of the three remaining if the winner is east; or the specific loser if ron).
         /// - Number of points lost by the two other players.
         /// </returns>
-        internal static Tuple<int, int> GetPoints(int fanCount, int fuCount, bool isTsumo, Winds playerWind)
+        internal static (int east, int notEast) GetPoints(int fanCount, int fuCount, bool isTsumo, Winds playerWind)
         {
             var v1 = 0;
             var v2 = 0;
@@ -282,7 +267,7 @@ namespace Gnoj_Ham_Library
                 }
             }
 
-            return new Tuple<int, int>(v1, v2);
+            return (v1, v2);
         }
 
         /// <summary>
