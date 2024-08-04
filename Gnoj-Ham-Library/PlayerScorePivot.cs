ec1f8a5e7c2fb57760e0dcad7a56ab4ad4e97123
Lpi <thelpi.v3@gmail.com>
2024-08-04 18:17:51 +0200

    clean PlayerScorePivot

--- Gnoj-Ham-Library/PlayerScorePivot.cs
+++ Gnoj-Ham-Library/PlayerScorePivot.cs
@@ -1,53 +1,52 @@
-﻿namespace Gnoj_Ham_Library
+﻿namespace Gnoj_Ham_Library;
+
+/// <summary>
+/// Represents a player score at the end of the game.
+/// </summary>
+public class PlayerScorePivot
 {
+    #region Embedded properties
+
     /// <summary>
-    /// Represents a player score at the end of the game.
+    /// Ranking.
     /// </summary>
-    public class PlayerScorePivot
+    public int Rank { get; }
+
+    /// <summary>
+    /// The player.
+    /// </summary>
+    public PlayerPivot Player { get; }
+
+    /// <summary>
+    /// Uma.
+    /// </summary>
+    public int Uma { get; }
+
+    /// <summary>
+    /// Final score; <see cref="PlayerPivot.Points"/> (only thousands) plus <see cref="Uma"/>.
+    /// </summary>
+    public int Score { get; }
+
+    #endregion Embedded properties
+
+    #region Constructors
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="player">The <see cref="Player"/> value.</param>
+    /// <param name="rank">The <see cref="Rank"/> value.</param>
+    /// <param name="uma">The <see cref="Uma"/> value.</param>
+    /// <param name="initialPoints">The initial points.</param>
+    internal PlayerScorePivot(PlayerPivot player, int rank, int uma, int initialPoints)
     {
-        #region Embedded properties
+        Player = player;
+        Rank = rank;
+        Uma = uma;
+        Score = ((player.Points - initialPoints) / 1000) + uma;
 
-        /// <summary>
-        /// Ranking.
-        /// </summary>
-        public int Rank { get; private set; }
-
-        /// <summary>
-        /// The player.
-        /// </summary>
-        public PlayerPivot Player { get; private set; }
-
-        /// <summary>
-        /// Uma.
-        /// </summary>
-        public int Uma { get; private set; }
-
-        /// <summary>
-        /// Final score; <see cref="PlayerPivot.Points"/> (only thousands) plus <see cref="Uma"/>.
-        /// </summary>
-        public int Score { get; private set; }
-
-        #endregion Embedded properties
-
-        #region Constructors
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        /// <param name="player">The <see cref="Player"/> value.</param>
-        /// <param name="rank">The <see cref="Rank"/> value.</param>
-        /// <param name="uma">The <see cref="Uma"/> value.</param>
-        /// <param name="initialPoints">The initial points.</param>
-        internal PlayerScorePivot(PlayerPivot player, int rank, int uma, int initialPoints)
-        {
-            Player = player;
-            Rank = rank;
-            Uma = uma;
-            Score = ((player.Points - initialPoints) / 1000) + uma;
-
-            Player.PermanentPlayer?.AddGameScore(this);
-        }
-
-        #endregion Constructors
+        Player.PermanentPlayer?.AddGameScore(this);
     }
+
+    #endregion Constructors
 }
