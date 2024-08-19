4fe26aff2d4374348148f0a225f3678c996cae4b
Lpi <thelpi.v3@gmail.com>
2024-08-19 10:53:17 +0200

    add tests

--- Gnoj-Ham-Library-Tests/AutoPlay_Tests.cs
+++ Gnoj-Ham-Library-Tests/AutoPlay_Tests.cs
@@ -8,13 +8,19 @@ public class AutoPlay_Tests
     {
         { 1000, new[] { ("CPU_1", 47700), ("CPU_0", 28000), ("CPU_3", 20000), ("CPU_2", 4300) } },
         { 666, new[] { ("CPU_0", 49000), ("CPU_1", 28500), ("CPU_3", 13000), ("CPU_2", 9500) } },
-        { 999999, new[] { ("CPU_1", 51700), ("CPU_3", 24900), ("CPU_2", 24100), ("CPU_0", -700) } }
+        { 999999, new[] { ("CPU_1", 51700), ("CPU_3", 24900), ("CPU_2", 24100), ("CPU_0", -700) } },
+        { 123456, new[] { ("CPU_1", 54500), ("CPU_0", 28700), ("CPU_2", 16900), ("CPU_3", -100) } },
+        { 789456, new[] { ("CPU_3", 38400), ("CPU_0", 22900), ("CPU_1", 20800), ("CPU_2", 17900) } },
+        { 187543, new[] { ("CPU_1", 35900), ("CPU_3", 23500), ("CPU_0", 23300), ("CPU_2", 17300) } }
     };
 
     [Theory]
     [InlineData(1000)]
     [InlineData(666)]
     [InlineData(999999)]
+    [InlineData(123456)]
+    [InlineData(789456)]
+    [InlineData(187543)]
     public void AutoPlay_GeneratesExpectedRound(int seed)
     {
         var random = new Random(seed);
