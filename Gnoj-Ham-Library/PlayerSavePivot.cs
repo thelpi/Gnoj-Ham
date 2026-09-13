c4daf5053c0bcc46c03ac2c036a26d71eaebea05
Lpi <thelpi.v3@gmail.com>
2026-09-13 11:25:28 +0200

    bugs on playersavepivot

--- Gnoj-Ham-Library/PlayerSavePivot.cs
+++ Gnoj-Ham-Library/PlayerSavePivot.cs
@@ -1,64 +1,71 @@
-﻿using System.Text.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Gnoj_Ham_Library;
 
 /// <summary>
 /// Player save file
 /// </summary>
-[Serializable]
 public class PlayerSavePivot
 {
     private const string SAVE_FILE_NAME = "save_file.dat";
 
-    private static string FullFileName => $"{Environment.CurrentDirectory}\\{SAVE_FILE_NAME}";
+    private static string FullFileName => Path.Combine(Environment.CurrentDirectory, SAVE_FILE_NAME);
 
-    /// <summary>
-    /// A marker to avoid double count.
-    /// </summary>
-    private bool InProgressGame { get; set; }
     /// <summary>
     /// Date of the first game (with at least one round completed).
     /// </summary>
+    [JsonInclude]
     public DateTime? FirstGame { get; private set; }
     /// <summary>
     /// Date of the most recent completed game (with at least one round completed).
     /// </summary>
+    [JsonInclude]
     public DateTime? LastGame { get; private set; }
     /// <summary>
     /// Number of games.
     /// </summary>
+    [JsonInclude]
     public int GameCount { get; private set; }
     /// <summary>
     /// Number of rounds.
     /// </summary>
+    [JsonInclude]
     public int RoundCount { get; private set; }
     /// <summary>
     /// Number of games by ranking position.
     /// </summary>
+    [JsonInclude]
     public int[] ByPositionCount { get; private set; } = new[] { 0, 0, 0, 0 };
     /// <summary>
     /// Number of riichi declarations.
     /// </summary>
+    [JsonInclude]
     public int RiichiCount { get; private set; }
     /// <summary>
     /// Number of bankrupts.
     /// </summary>
+    [JsonInclude]
     public int BankruptCount { get; private set; }
     /// <summary>
     /// Number of tsumo declarations.
     /// </summary>
+    [JsonInclude]
     public int TsumoCount { get; private set; }
     /// <summary>
     /// Number of ron declarations.
     /// </summary>
+    [JsonInclude]
     public int RonCount { get; private set; }
     /// <summary>
     /// Number of yakuman hands.
     /// </summary>
+    [JsonInclude]
     public int YakumanCount { get; private set; }
     /// <summary>
     /// Number of opened hands.
     /// </summary>
+    [JsonInclude]
     public int OpenedHandCount { get; private set; }
 
     /// <summary>
@@ -102,25 +109,20 @@ public class PlayerSavePivot
     }
 
     /// <summary>
-    /// 
+    /// Updates the statistics with the outcome of a round (and, if applicable, the end of the game), then saves the file.
     /// </summary>
-    /// <param name="endOfRoundInformations"></param>
-    /// <param name="isRon"></param>
-    /// <param name="humanIsRiichi"></param>
-    /// <param name="humanIsConcealed"></param>
-    /// <param name="scoreIndexPosition"></param>
-    /// <param name="meScore"></param>
-    /// <returns></returns>
+    /// <param name="endOfRoundInformations">The end-of-round outcome.</param>
+    /// <param name="isRon">Indicates the human player won by ron (as opposed to tsumo); irrelevant if the human player didn't win.</param>
+    /// <param name="humanIsRiichi">Indicates the human player was riichi during this round.</param>
+    /// <param name="humanIsConcealed">Indicates the human player's hand was still concealed at the end of the round.</param>
+    /// <param name="scoreIndexPosition">The human player's rank at the end of the game; irrelevant if the game isn't over.</param>
+    /// <param name="meScore">The human player's current points; irrelevant if the game isn't over.</param>
+    /// <returns>An error message if the save failed; <c>Null</c> otherwise.</returns>
     internal string? UpdateAndSave(EndOfRoundInformationsPivot endOfRoundInformations,
         bool isRon, bool humanIsRiichi, bool humanIsConcealed, int scoreIndexPosition, int meScore)
     {
         var now = DateTime.Now;
         var pHand = endOfRoundInformations.PlayersInfo?.FirstOrDefault(_ => !_.IsCpu);
-
-        if (!InProgressGame)
-        {
-            InProgressGame = true;
-        }
 
         ++RoundCount;
 
@@ -151,7 +153,6 @@ public class PlayerSavePivot
             }
             LastGame = now;
             ++GameCount;
-            InProgressGame = false;
         }
 
         // save at each round (so rounds on given up games are kept)
